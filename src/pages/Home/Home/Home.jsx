ae39509dbc166e7191cac435460522374e68d795
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-08-27 21:18:11 +0600

    low price offer section is added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Categories from '../Categories/Categories';
+import LowPriceOffer from '../lowPriceOffer/lowPriceOffer';
+
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <Categories/>
+            <LowPriceOffer/>
         </div>
     );
 };
