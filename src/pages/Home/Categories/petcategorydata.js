721a24ba01138a0d7098a2a8030a386f12f3ceb7
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-08-27 20:54:09 +0600

    pet categories section is added

---
+++ src/pages/Home/Categories/petcategorydata.js
@@ -0,0 +1,42 @@
+import dog from "../../../assets/categories/dog-cartoon.png"
+import cat from "../../../assets/categories/cat-cartoon.png"
+import fish from "../../../assets/categories/fish-cartoon.png"
+import rabbit from "../../../assets/categories/rabbit-cartoon.png"
+import smallpet from "../../../assets/categories/mouse-cartoon.png"
+import birds from "../../../assets/categories/bird-cartoon.png"
+
+const categories = [
+    {
+      id: 1,
+      name: "Dog",
+      image: dog
+    },
+    {
+      id: 2,
+      name: "Fish",
+      image: fish
+    },
+    {
+      id: 3,
+      name: "Cat",
+      image: cat
+    },
+    {
+      id: 4,
+      name: "Rabbit",
+      image: rabbit
+    },
+    {
+      id: 5,
+      name: "Birds",
+      image: birds
+    },
+    {
+      id: 6,
+      name: "Small Pet",
+      image: smallpet
+    }
+  ];
+  
+  export default categories;
+  
