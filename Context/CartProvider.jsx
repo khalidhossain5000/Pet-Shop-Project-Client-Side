46453383d7ade1145fce10497bf8632a43a12a27
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-09-06 15:43:40 +0600

    cart added toast notifications is added

--- Context/CartProvider.jsx
+++ Context/CartProvider.jsx
@@ -1,17 +1,16 @@
 // CartProvider.jsx
 import React, { useState } from "react";
 import { CartContext } from "./CartContext";
-
+import { toast } from "react-hot-toast";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
-
 
 import useAuth from "../Hooks/useAuth";
 
 const CartProvider = ({ children }) => {
-  const axiosSecure=useAxiosSecure()
-  const {user}=useAuth()
-  
-  console.log(user,axiosSecure);
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+
+  console.log(user, axiosSecure);
   // Cart state
   const [cart, setCart] = useState([]);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -19,25 +18,35 @@ const CartProvider = ({ children }) => {
     setIsDrawerOpen(open);
   };
   // inside CartProvider
-  
-  const addToCart=async(petData)=>{
-    
-    const cartData={
-      email:user?.email,
-      item:{
-        petName:petData?.petName,
-        petPrice:petData?.price,
-        petImage:petData?.petPic
-      }
-    }
 
-    const res=await axiosSecure.post('/cart',cartData)
-    console.log("this is res",res)
-    setCart(res.data)
-    alert("Item Added To cart successfully")
-     setIsDrawerOpen(true);
-  }
+  const addToCart = async (petData) => {
+    const cartData = {
+      email: user?.email,
+      item: {
+        petName: petData?.petName,
+        petPrice: petData?.price,
+        petImage: petData?.petPic,
+      },
+    };
 
+    const res = await axiosSecure.post("/cart", cartData);
+    console.log("this is res", res);
+    setCart(res.data);
+    toast.success(`User Registered SuccessFully`, {
+      className: "w-[300px] h-[100px] text-xl font-bold ",
+      removeDelay: 1000,
+      iconTheme: {
+        primary: "#16061e",
+        secondary: "#ef54e2",
+      },
+      style: {
+        border: "1px solid #08086c",
+        color: "white",
+        backgroundImage: "linear-gradient(to bottom right, #050342,#01c3f4 )",
+      },
+    });
+    setIsDrawerOpen(true);
+  };
 
   return (
     <CartContext
@@ -46,8 +55,7 @@ const CartProvider = ({ children }) => {
         setCart,
         isDrawerOpen,
         toggleDrawer,
-        addToCart
-        
+        addToCart,
       }}
     >
       {children}
