b8e21f7167050de84f08867c7212e2bed5e11d6b
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-09-29 19:02:15 +0600

    my order stats added in profile dashboard

--- src/pages/UserProfilePages/ProfileHome/ProfileHome.jsx
+++ src/pages/UserProfilePages/ProfileHome/ProfileHome.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import bannerImg from "../../../assets/Bgimg/profile-bg.jpg";
 import useAuth from "../../../../Hooks/useAuth";
 import useRole from "../../../../Hooks/useRole";
+import { IoIosTrendingUp } from "react-icons/io";
+import { FiMoreVertical, FiUser } from "react-icons/fi";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
+import Loading from "../../../Shared/Loading/Loading";
+import { Link } from "react-router";
 
 const ProfileHome = () => {
   const { user } = useAuth();
   const { role } = useRole();
+  const axiosSecure=useAxiosSecure()
+    const { data: totalOrders = 0, isLoading,  } = useQuery({
+    queryKey: ["total-orders", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/orders/count?email=${user?.email}`);
+      return res.data.totalOrders;
+    },
+  });
+  if (isLoading) return <Loading/>
+  console.log(totalOrders,'This is total order ');
   return (
     <div className=" relative">
       {/* my style start here  */}
@@ -38,6 +54,62 @@ const ProfileHome = () => {
         </div>
       </div>
       {/* profle more details will be add later */}
+      <div className="container mx-auto">
+        {/* TOTAL ORDERS CARD*/}
+        <div className="relative w-full max-w-xs sm:max-w-lg  rounded-xl p-5 lg:p-9 overflow-hidden bg-gradient-to-r from-green-600 to-emerald-500 text-white shadow-2xl">
+          {/* --- Background Trend Graphic --- */}
+          <div className="absolute inset-0 flex items-center justify-start ml-24 opacity-10">
+            <IoIosTrendingUp className="w-40 h-40 text-white rotate-12" />
+          </div>
+          {/* --- Card Content Layout --- */}
+          <div className="relative flex flex-col  justify-between h-full">
+            {/* TOP SECTION: Title and Icon Badge */}
+            <div className="flex flex-col justify-between items-start">
+
+
+                <div className="flex items-center justify-between w-full">
+
+              <h2 className="text-xl md:text-2xl lg:text-3xl font-medium  lg:font-bold tracking-wide">Total Orders</h2>
+              {/* User Icon Badge  */}
+              <div className="p-3 rounded-full bg-green-700/50 backdrop-blur-sm shadow-inner">
+                <FiUser className="w-5 h-5" />
+              </div>
+                </div>
+
+
+
+
+
+              
+              {/* MIDDLE SECTION: Large Metric Number */}
+              <div className="flex-1 flex items-center pt-2">
+                <p className="text-2xl md:text-3xl lg:text-6xl font-bold lg:font-extrabold ">{totalOrders}</p>
+              </div>
+              {/* BOTTOM SECTION: Percentage Change and Menu Icon */}
+              <div className="flex flex-col  justify-end mt-2 space-y-2 lg:space-y-3">
+                {/* Percentage Change Pill and Label */}
+            
+                 
+                  <span className="text-sm font-light opacity-90">
+                    All Time
+                  </span>
+            
+
+                {/* Menu Icon (Vertical Dots) */}
+                <Link to={'/all-pets'}>
+                <button
+                  className="px-3 py-1 lg:py-2 cursor-pointer rounded-full text-white/90 hover:bg-green-700 transition flex items-center bg-light-accent/60"
+                  
+                  onClick={() => console.log("Menu clicked")}
+                >
+                  Shop More
+                </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
