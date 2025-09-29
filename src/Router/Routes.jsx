add88b323d076fb424e25033a54902818c781159
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-09-29 19:54:56 +0600

    update profile page added here

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -26,6 +26,7 @@ import Orders from "../pages/AdminDashBoard/Orders/Orders";
 import ProfileDashboard from "../layouts/UserProfileDashboard/ProfileDashboard";
 import ProfileHome from "../pages/UserProfilePages/ProfileHome/ProfileHome";
 import MyOrders from "../pages/UserProfilePages/MyOrders/MyOrders";
+import ProfileUpdate from "../pages/UserProfilePages/ProfileUpdate/ProfileUpdate";
 
 
 
@@ -156,6 +157,10 @@ export const router = createBrowserRouter([
       {
         path:'my-orders',
         Component:MyOrders
+      },
+      {
+        path:'update',
+        Component:ProfileUpdate
       }
     ]
   }
