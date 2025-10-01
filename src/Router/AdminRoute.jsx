0a597ed3760cd871de53a975e743741d92d96a8e
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-10-01 20:25:14 +0600

    admin route added

---
+++ src/Router/AdminRoute.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Navigate, useLocation } from "react-router";
+import useAuth from "../../Hooks/useAuth";
+import useRole from "../../Hooks/useRole";
+import Loading from "../Shared/Loading/Loading";
+
+const AdminRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const { role, roleLoading } = useRole();
+  const location = useLocation();
+  if (loading || roleLoading) return <Loading />;
+  if (!user || role !== "admin") {
+    return (
+      <Navigate state={{ from: location.pathname }} to="/"></Navigate>
+    );
+  }
+//forbidden page will be added later 
+  return children;
+};
+
+export default AdminRoute;
